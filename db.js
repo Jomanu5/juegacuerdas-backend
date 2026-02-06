0b36e44055b38fb002cbe71ffeda230a22569499
José Manuel <josemanuelharri@gmail.com>
2026-02-06 20:56:48 -0300

    otro mas

--- db.js
+++ db.js
@@ -1,18 +1,12 @@
 // db.js
 import { PrismaClient } from '@prisma/client';
 
-//  Forzamos a Prisma a usar la variable de entorno de Render
+// En Prisma 7, el constructor debe ir vacío si la URL está en el sistema.
+const prisma = new PrismaClient();
+
+// 🔍 Agregamos un log de depuración (solo se verá en Render)
 if (!process.env.DATABASE_URL) {
-  console.error("❌ ERROR: La variable DATABASE_URL no está definida en el sistema.");
+  console.error("⚠️ ALERTA: La variable DATABASE_URL no se detecta en el sistema.");
 }
-
-
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
 
 export default prisma;
